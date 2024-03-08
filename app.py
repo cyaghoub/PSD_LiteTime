c8793aaf62c0a22dbfa7b98852a0fb1a9faff473
cyaghoub <cyaghoub@student.touro.edu>
2024-03-07 22:15:12 -0500

    Added index.html and templates

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return '<h1>Hello, World!</h1>'
+def index():
+    user = {'username': 'Chana Bina'}
+    return render_template('index.html', user=user)
+
+if __name__ == '__main__':
+    app.run(debug=True)

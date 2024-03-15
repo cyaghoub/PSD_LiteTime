683d7eb0c450a41bf604a9e5fe05e63b79868153
cyaghoub <cyaghoub@student.touro.edu>
2024-03-14 21:14:35 -0400

    Integrated sunset api, displays sunset for current date and hardcoded coordinates

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+SUNSET_API_URL = 'https://api.sunrise-sunset.org/json'
+
 @app.route('/')
 def index():
-    user = {'username': 'Chana Bina'}
-    return render_template('index.html', user=user)
+    #example of nyc coordinates
+    latitude = 40.7128
+    longitude = -74.0060
+
+    #request to sunset api
+    params = {'lat': latitude, 'lng': longitude}
+    response = requests.get(SUNSET_API_URL, params=params)
+    data = response.json()
+
+    #display response(sunset time)
+    sunset_time = data['results']['sunset']
+
+    return render_template('index.html', sunset_time=sunset_time)
 
 if __name__ == '__main__':
     app.run(debug=True)
